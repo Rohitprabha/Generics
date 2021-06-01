ccadb0ac60f630c972c4219fb2d950dd05e0399b
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-01 16:01:14 +0530

    Add files via upload

---
+++ Generic Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generic_Method
+{
+    public class ClassA
+    {
+        public void Display<T, S>(T msg, S value)         //Generic Method
+        {
+            Console.WriteLine("{0} : {1}", msg, value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA a = new ClassA();
+            a.Display<string, int>("Integer", 122);
+            a.Display<string, char>("Character", 'H');
+            a.Display<string, float>("Float", 25.87f);
+        }
+    }
+}
