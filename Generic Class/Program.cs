4c8a73976a9210a88eac9e3cb37abf6eb1558aea
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-06-01 15:59:46 +0530

    Add files via upload

---
+++ Generic Class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Generic_Class
+{
+    public class Generic_Class<T>
+    {
+        private T temp;
+        public Generic_Class(T temp)
+        {
+            this.temp = temp;
+        }
+        public void Display()
+        {
+            Console.WriteLine(temp);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic_Class<int> Obj = new Generic_Class<int>(10);
+            Obj.Display();
+            Generic_Class<string> Obj1 = new Generic_Class<string>("Rohit");
+            Obj1.Display();
+            Generic_Class<float> Obj2 = new Generic_Class<float>(1.004f);
+            Obj2.Display();
+
+        }
+    }
+}
